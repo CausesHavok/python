e329e6e23820cdadb9341636f197a7a73fc3c2fb
Magnus Sigersdal <Sigersdal@hotmail.com>
2023-05-22 09:03:07 +0200

    Pangram solution

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -5,6 +5,7 @@ def is_pangram(sentence: str):
     :param sentece str: A sentence
     :return bool: Is sentence a pangram
     Determines if sentence is a pangram
+    Works by converting both the alphabet and the sentence to a set (unique list of characters) and comparing them
     """
-    alphabet = "abcdefghijklmnopqrstuvwxyz" 
-    return all(char in sentence.lower() for char in alphabet)
+    return set("abcdefghijklmnopqrstuvwxyz").issubset(sentence.lower()) # alternatively return all(char in sentence.lower() for char in alphabet)
+
