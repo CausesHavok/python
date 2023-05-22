6ece9009568f2536b2f7bc08fe88fd00472cf3b1
Magnus <sigersdal.membership@outlook.com>
2023-05-22 07:00:28 +0200

    pangram solution

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,10 @@
-def is_pangram(sentence):
-    pass
+"""Function for determining pangrams"""
+def is_pangram(sentence: str):
+    """Determine if sentence is pangram
+    
+    :param sentece str: A sentence
+    :return bool: Is sentence a pangram
+    Determines if sentence is a pangram
+    """
+    alphabet = "abcdefghijklmnopqrstuvwxyz" 
+    return all(char in sentence.lower() for char in alphabet)
