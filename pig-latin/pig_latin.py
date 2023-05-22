c41e82ff752c29dadc0a6ae11e7d3837867365f2
Magnus Sigersdal <Sigersdal@hotmail.com>
2023-05-22 12:00:23 +0200

    pig latin updated solution

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,4 +1,3 @@
-import re
 """Functions for translating English into pig latin"""
 def translate(text:str):
     """Translate text into pig latin
@@ -7,41 +6,47 @@ def translate(text:str):
     :return str: Text in pig latin
     Translates text into pig latin following rules found on https://exercism.org/tracks/python/exercises/pig-latin
     """
-    pig_latin_sentence = ""
+    pig_latin_words = []
     for word in text.split():
-        pig_latin_sentence += translate_word(word) + " "
+        pig_latin_words.append(translate_word(word))
 
-    return pig_latin_sentence[:-1]
+    return ' '.join(pig_latin_words)
+
+def find_vowel(text:str):
+    """Finds the fist lower case vowel in a text
+    
+    :param text str: Indput text
+    :return int: index of the first lower case vowel in input text
+    """
+    vowels_sounds = ("aeiouy")
+    index = 0
+    for char in text:
+        if char in vowels_sounds:
+            return index
+        index += 1
 
 
-def translate_word(text):
+def translate_word(text:str):
     """Translate a single word into pig latin
     
     :param text str: A word
     :return str: A word in pig latin
     Translates a word into pig latin following rules found on https://exercism.org/tracks/python/exercises/pig-latin
     """
+    consonant_sounds = ("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z")
+    vowels_sounds = ("a","e","i","o","u")
 
+    pig_latin = ""
     #Rule 1
-    if text[0] in ['a','e','i','o','u'] or text.startswith("xr") or text.startswith("yt"):
-        return text+"ay"
+    if text.startswith(vowels_sounds) or text.startswith("xr") or text.startswith("yt"):
+        pig_latin = text
 
-    #Rule 3
-    matchvar= re.match("[bcdfghjklmnpqrstvwxz]?qu",text)
-    if matchvar:
-        lengthvar = len(matchvar.group(0))
-        return text[lengthvar:] + text[:lengthvar] + "ay"
+    #Rule 2-4
+    elif text.startswith(consonant_sounds):
+        first_vowel = find_vowel(text[1:]) + 1
+        if text[first_vowel-1] == "q" and text[first_vowel] == "u": # Rule 3
+            pig_latin = text[first_vowel+1:] + text[:first_vowel+1]
+        else: #rule 2 and 4
+            pig_latin = text[first_vowel:] + text[:first_vowel]
 
-    #Rule 4
-    matchvar= re.match("[bcdfghjklmnpqrstvwxz]+y",text)
-    if matchvar:
-        lengthvar = len(matchvar.group(0))
-        return text[lengthvar-1:] + text[:lengthvar-1] + "ay"
-    
-    #Rule 2
-    matchvar = re.match("y?[bcdfghjklmnpqrstvwxz]*",text) #relies on the fact that there must be a consonant or y
-    if matchvar:
-        lengthvar = len(matchvar.group(0))
-        return text[lengthvar:] + text[:lengthvar] + "ay"
-
-    return "rule5"
+    return pig_latin + "ay"
