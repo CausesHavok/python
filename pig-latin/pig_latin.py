4b84c28a582432b5fcb8c0ae2ec1bf200add2121
Magnus Sigersdal <Sigersdal@hotmail.com>
2023-05-22 08:17:20 +0200

    pig latin solution

---
+++ pig-latin/pig_latin.py
@@ -0,0 +1,16 @@
+"""Translate a word into pig-latin"""
+def translate(text:str):
+    """Translate a word into pig-latin
+    
+    :param text str: A word
+    :return str: A pig-latin translation of the input word
+
+    Translate a word into pig-latin based on the following rules
+    Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of the word. Please note that "xr" and "yt" at the beginning of a word make vowel sounds (e.g. "xray" -> "xrayay", "yttria" -> "yttriaay").
+    Rule 2: If a word begins with a consonant sound, move it to the end of the word and then add an "ay" sound to the end of the word. Consonant sounds can be made up of multiple consonants, a.k.a. a consonant cluster (e.g. "chair" -> "airchay").
+    Rule 3: If a word starts with a consonant sound followed by "qu", move it to the end of the word, and then add an "ay" sound to the end of the word (e.g. "square" -> "aresquay").
+    Rule 4: If a word contains a "y" after a consonant cluster or as the second letter in a two letter word it makes a vowel sound (e.g. "rhythm" -> "ythmrhay", "my" -> "ymay").
+    """
+
+    
+    pass
