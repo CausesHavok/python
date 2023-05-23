127c6e9f9d31b01471055eb7f8cc2e089713754a
Magnus <sigersdal.membership@outlook.com>
2023-05-23 07:59:26 +0200

    isbn partial solution

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,6 +1,9 @@
 def is_valid(isbn:str):
-
+    
     scrubbed_isbn = isbn.replace('-', '').lower()
+    if len(isbn)!=10:
+        return False
+    
     control_sum = 0
     digit_position = 10
     for digit in scrubbed_isbn:
@@ -10,3 +13,6 @@ def is_valid(isbn:str):
             control_sum += int(digit)*digit_position
         digit_position -= 1
     return control_sum % 11 == 0
+
+
+if len(isbn)!=:
